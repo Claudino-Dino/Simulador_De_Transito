d6576280586727e5d4546f2001b599f3eceff282
Romerson Claudino <rom3rsong0@gmail.com>
2025-04-22 08:05:32 -0300

    Commit para atualizar semaforo e ListaCircular

--- Simulador/src/main/java/simulador/semaforo/ControladorSemaforos.java
+++ Simulador/src/main/java/simulador/semaforo/ControladorSemaforos.java
@@ -1,8 +1,27 @@
 package simulador.semaforo;
 
+import simulador.estruturas.ListaCircular;
+
 public class ControladorSemaforos {
-    public void aplicarControle(){
+    public ListaCircular<Enum> modosOreperacao;
+    public Enum modoOperacaoAtual;
+
+    public ControladorSemaforos() {
+        modoOperacaoAtual = modosOreperacao.getAtual();
+
+        while (!modoOperacaoAtual.equals("CICLO_FIXO")){
+            setModoAtual();
+        }
+    }
+
+    public void aplicarControle() {
         System.out.println("Controle de semáforos aplicado.");
+    }
+
+    public Enum setModoAtual() {
+        this.modosOreperacao.proximo();
+        this.modoOperacaoAtual = this.modosOreperacao.getAtual();
+        return modoOperacaoAtual;
     }
 
 }
